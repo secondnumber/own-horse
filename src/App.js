71f7b47ce6775415b9eaf1e42b134333b0cd6a56
Yulya Vorontsova <secondnumber01@gmail.com>
2020-04-26 19:47:09 +0300

    feat(product): add fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
+import Slider from './components/Slider/Slider';
 
 const App = () => (
-  <div>Post</div>
+  <div>
+    <Header />
+    <Slider />
+  </div>
 );
-
 export default App;
