13728b75a8d66d44f20e16ad33c7530d461fb0c5
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-05-06 12:56:57 +0300

    feat(product): make stylization of single game section

---
+++ src/components/Games/GameList/GameList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './GameList.module.scss';
+
+const GameList = (props) => {
+  return (
+    <div>
+      <img className={classes.image} src={props.imageSmall} />
+      <h1 className={classes.header}>{props.name}</h1>
+      <p className={classes.date}>{props.date}</p>
+    </div>
+  );
+};
+
+export default GameList;
--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import classes from './Games.module.scss';
+import GameList from './GameList/GameList';
 import Game from './Game/Game';
-import News from './News/News';
 import ArrowLeft from './left-arrow.svg';
 import ArrowRight from './right-arrow.svg';
 
 const Games = (props) => {
   let gamesList = props.games.map((game) => (
-    <Game name={game.name} date={game.date} imageSmall={game.src} />
+    <GameList name={game.name} date={game.date} imageSmall={game.src} />
   ));
 
   return (
     <div className={classes.wrapper}>
-        <div className={classes.headers}>
-      <h2 className={classes.subtitle}>Coming soon
+      <div className={classes.headers}>
+        <h2 className={classes.subtitle}>
+          Coming soon
           <h2 className={classes.title}>Upcoming games</h2>
-      </h2>
-        </div>
+        </h2>
+      </div>
       <div className={classes.block}>
         <button className={classes.button}>
           <img src={ArrowLeft} />
@@ -26,7 +27,7 @@ const Games = (props) => {
           <img src={ArrowRight} />
         </button>
       </div>
-      <News />
+      <Game />
     </div>
   );
 };
