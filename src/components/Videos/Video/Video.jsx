4ccf0b7857454aeea05585a72f7f87903b9ab7ce
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-05-09 10:16:34 +0300

    fix(product): fix problem with video size in video section

--- src/components/Videos/Video/Video.jsx
+++ src/components/Videos/Video/Video.jsx
@@ -3,7 +3,7 @@ import classes from './Video.module.scss';
 
 const Video = (props) => {
   return (
-    <video className={classes.video} width="600" controls>
+    <video className={classes.video} width="500" controls>
       <source src={props.file} type="video/mp4" />
     </video>
   );
