808888621bdf0825da1d3b366c38d5fe4ddcff38
Yulya Vorontsova <secondnumber01@gmail.com>
2020-05-20 21:04:21 +0300

    feat(product): make price list page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import MainPage from './components/pages/MainPage/MainPage';
 import NotFoundPage from './components/pages/NotFoundPage/NotFoundPage';
 import AboutPageContainer from './components/pages/AboutPage/AboutPageContainer';
 import TeamPageContainer from './components/pages/TeamPage/TeamPageContainer';
+import PriceListPageContainer from './components/pages/PriceListPage/PriceListPageContainer';
 
 const App = (props) => (
   <div>
     {/* <MainPage /> */}
     {/* <NotFoundPage /> */}
     {/* <AboutPageContainer /> */}
-    <TeamPageContainer />
+    {/* <TeamPageContainer /> */}
+    <PriceListPageContainer />
   </div>
 );
 export default App;
