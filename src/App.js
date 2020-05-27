9d5629331e9617036b6c2d791079fbdfe87e6992
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-05-27 13:36:17 +0300

    feat(product): change style on testimonial page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import './App.scss';
-import MainPage from './components/pages/MainPage/MainPage';
-import NotFoundPage from './components/pages/NotFoundPage/NotFoundPage';
-import AboutPageContainer from './components/pages/AboutPage/AboutPageContainer';
-import TeamPageContainer from './components/pages/TeamPage/TeamPageContainer';
-import PriceListPageContainer from './components/pages/PriceListPage/PriceListPageContainer';
-import GalleryPageContainer from './components/pages/GalleryPage/GalleryPageGontainer';
-import SingleImagePageContainer from './components/pages/SingleImagePage/SingleImagePageGontainer';
-import TestimonialsPageContainer from './components/pages/TestimonialsPage/TestimonialsPageContainer';
+import TestimonialsPageContainer from './components/Pages/TestimonialsPage/TestimonialsPageContainer';
 
 const App = (props) => (
   <div>
