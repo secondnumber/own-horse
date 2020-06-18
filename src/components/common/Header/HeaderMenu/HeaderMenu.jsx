f80f06a1becbe8a1a15a350b58c0fb0efffdb816
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-18 10:46:50 +0300

    feat(product): start dropdown menu

--- src/components/common/Header/HeaderMenu/HeaderMenu.jsx
+++ src/components/common/Header/HeaderMenu/HeaderMenu.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import classes from './HeaderMenu.module.scss';
 import { NavLink } from 'react-router-dom';
+import DropdownMenuContainer from './DropdownMenu/DropdownMenuContainer';
 
 const HeaderMenu = (props) => (
-  <li>
-    <NavLink to={props.menuLink} className={classes.navItem} activeClassName={classes.navItemActive}>
-      {props.menuItem}
-    </NavLink>
-  </li>
-);
+    <li className={classes.item}>
+      <NavLink
+        to={props.menuLink}
+        className={classes.link}
+        activeClassName={classes.linkActive}
+      >
+        {props.menuItem}
+      </NavLink>
+      {props.nestedMenu && <DropdownMenuContainer nestedMenu={props.nestedMenu}/>}
+    </li>
+  );
 
 export default HeaderMenu;
--- src/redux/reducers/navigationReducer.js
+++ src/redux/reducers/navigationReducer.js
@@ -1,9 +1,21 @@
 let initialState = {
   navMenuItems: [
     { id: 1, menuItem: 'Homepage', menuLink: '' },
-    { id: 2, menuItem: 'Pages', menuLink: 'pages' },
+    {
+      id: 2,
+      menuItem: 'Pages',
+      menuLink: 'pages',
+      nestedMenu: [
+        { id: 1, nestedMenuItem: 'About', nestedMenuLink: 'pages' },
+        { id: 2, nestedMenuItem: 'Faq', nestedMenuLink: 'faq' },
+        { id: 3, nestedMenuItem: 'Team', nestedMenuLink: 'team' },
+        { id: 4, nestedMenuItem: 'Pricing plans', nestedMenuLink: 'price' },
+        { id: 5, nestedMenuItem: 'Testimonials', nestedMenuLink: 'testimonials' },
+        { id: 6, nestedMenuItem: 'Statistic', nestedMenuLink: 'statistic' },
+      ],
+    },
     { id: 3, menuItem: 'Gallery', menuLink: 'gallery' },
-    { id: 4, menuItem: 'Games', menuLink: 'games' },
+    { id: 4, menuItem: 'Games', menuLink: 'games', },
     { id: 5, menuItem: 'Blog', menuLink: 'blog' },
     { id: 6, menuItem: 'Contacts', menuLink: 'contacts' },
   ],
