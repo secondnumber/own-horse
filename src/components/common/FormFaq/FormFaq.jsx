ce3d847596f6bed384e66e5cb22a30c11b658609
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-23 13:47:16 +0300

    feat(product): make faq page without toggle

--- src/components/common/FormFaq/FormFaq.jsx
+++ src/components/common/FormFaq/FormFaq.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import classes from './FormFaq.module.scss';
 import Button from '../Button/Button';
 
-const FormContact = () => (
+const FormFaq = () => (
   <div className={classes.block}>
     <form action="">
-      <input className={classes.input} type="text" placeholder='full name'/>
+      <input className={classes.input} type="text" placeholder='your name'/>
       <input className={classes.input} type="text" placeholder='email address'/>
-      <input className={classes.input} type="text" placeholder='phone number'/>
       <input className={classes.input} type="text" placeholder='subject'/>
       <textarea
         className={classes.textarea}
@@ -15,11 +14,11 @@ const FormContact = () => (
         id=""
         cols="30"
         rows="10"
-        placeholder='your message...'
+        placeholder='your question'
       ></textarea>
       <Button />
     </form>
   </div>
 );
 
-export default FormContact;
+export default FormFaq;
--- src/redux/reducers/faqReducer.js
+++ src/redux/reducers/faqReducer.js
@@ -1,23 +1,102 @@
 let initialState = {
-  generalQuestions: [
-    {id: 1, title: 'PORK KEVIN BURGDOGGEN SWINE?', content: 'Drumstick meatloaf capicola short ribs, pork chop cupim kielbasa jerky porchetta pancetta chicken pastrami turkey tenderloin sausage. Pig buffalo alcatra meatball.'},
-    {id: 2, title: 'PASTRAMI PANCETTA BOUDIN MEATLOAF TONGUE?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-    {id: 3, title: 'PORCHETTA PORK BELLY HAMBURGER?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-    {id: 4, title: 'SHORT RIBS PIG TONGUE SIRLOIN?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-    {id: 5, title: 'PIG TONGUE SIRLOIN PORK BELLY BALL?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
+  questionsList: [
+    {
+      id: 1,
+      title: 'general Questions',
+      list: [
+        {
+          id: 1,
+          caption: 'PORK KEVIN BURGDOGGEN SWINE?',
+          content:
+            'Drumstick meatloaf capicola short ribs, pork chop cupim kielbasa jerky porchetta pancetta chicken pastrami turkey tenderloin sausage. Pig buffalo alcatra meatball.',
+        },
+        {
+          id: 2,
+          caption: 'PASTRAMI PANCETTA BOUDIN MEATLOAF TONGUE?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+        {
+          id: 3,
+          caption: 'PORCHETTA PORK BELLY HAMBURGER?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+        {
+          id: 4,
+          caption: 'SHORT RIBS PIG TONGUE SIRLOIN?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+        {
+          id: 5,
+          caption: 'PIG TONGUE SIRLOIN PORK BELLY BALL?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'financial Topics',
+      list: [
+        {
+          id: 1,
+          caption: 'PORK KEVIN BURGDOGGEN SWINE?',
+          content:
+            'Drumstick meatloaf capicola short ribs, pork chop cupim kielbasa jerky porchetta pancetta chicken pastrami turkey tenderloin sausage. Pig buffalo alcatra meatball.',
+        },
+        {
+          id: 2,
+          caption: 'PASTRAMI PANCETTA BOUDIN MEATLOAF TONGUE?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+        {
+          id: 3,
+          caption: 'PORCHETTA PORK BELLY HAMBURGER?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+        {
+          id: 4,
+          caption: 'SHORT RIBS PIG TONGUE SIRLOIN?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+        {
+          id: 5,
+          caption: 'PIG TONGUE SIRLOIN PORK BELLY BALL?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: 'otherQuestions',
+      list: [
+        {
+          id: 1,
+          caption: 'PORK KEVIN BURGDOGGEN SWINE?',
+          content:
+            'Drumstick meatloaf capicola short ribs, pork chop cupim kielbasa jerky porchetta pancetta chicken pastrami turkey tenderloin sausage. Pig buffalo alcatra meatball.',
+        },
+        {
+          id: 2,
+          caption: 'PASTRAMI PANCETTA BOUDIN MEATLOAF TONGUE?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+        {
+          id: 3,
+          caption: 'PORCHETTA PORK BELLY HAMBURGER?',
+          content:
+            'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.',
+        },
+      ],
+    },
   ],
-  financialTopics: [
-    {id: 1, title: 'PORK KEVIN BURGDOGGEN SWINE?', content: 'Drumstick meatloaf capicola short ribs, pork chop cupim kielbasa jerky porchetta pancetta chicken pastrami turkey tenderloin sausage. Pig buffalo alcatra meatball.'},
-    {id: 2, title: 'PASTRAMI PANCETTA BOUDIN MEATLOAF TONGUE?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-    {id: 3, title: 'PORCHETTA PORK BELLY HAMBURGER?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-    {id: 4, title: 'SHORT RIBS PIG TONGUE SIRLOIN?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-    {id: 5, title: 'PIG TONGUE SIRLOIN PORK BELLY BALL?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-  ],
-  otherQuestions: [
-    {id: 1, title: 'PORK KEVIN BURGDOGGEN SWINE?', content: 'Drumstick meatloaf capicola short ribs, pork chop cupim kielbasa jerky porchetta pancetta chicken pastrami turkey tenderloin sausage. Pig buffalo alcatra meatball.'},
-    {id: 2, title: 'PASTRAMI PANCETTA BOUDIN MEATLOAF TONGUE?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-    {id: 3, title: 'PORCHETTA PORK BELLY HAMBURGER?', content: 'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel.'},
-  ]
 };
 
 const faqReducer = (state = initialState, action) => {
