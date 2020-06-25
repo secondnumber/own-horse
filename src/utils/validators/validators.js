dc341b11215e03b57631cd451ed90e9f5826e0b0
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-25 12:39:40 +0300

    feat(product): add redux-form with validation in contacts form

--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -1,8 +1,24 @@
-export const validate = (values) => {
+export const validateEmail = (values) => {
   if (!values) {
     return 'Please, enter e-mail address';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values)) {
     return 'Invalid email address';
+  } else {
+    return undefined;
+  }
+};
+
+export const required = (value) => {
+  if (value) {
+    return undefined;
+  } else {
+    return 'Field is required';
+  }
+};
+
+export const maxLengthCreator = (maxLength) => (value) => {
+  if (value && value.length > maxLength) {
+    return `Max length is ${maxLength}`;
   } else {
     return undefined;
   }
