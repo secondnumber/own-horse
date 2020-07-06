4c005855108452121716732edda540d8bdf23293
Yulya Vorontsova <secondnumber01@gmail.com>
2020-07-06 14:57:55 +0300

    feat(product): make search form

--- src/components/Pages/BlogPage/BlogSearch/BlogSearch.jsx
+++ src/components/Pages/BlogPage/BlogSearch/BlogSearch.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './BlogSearch.module.scss';
+import Search from '../../../common/Search/Search';
 
 const BlogSearch = (props) => {
   return (
     <div>
       <h2 className={classes.header}>SEARCH ON WEBSITE</h2>
+      <Search />
     </div>
   );
 };
